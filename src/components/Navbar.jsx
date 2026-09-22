ba1e1f3fc2bd220ef3ac33f2a49d253ee90c4def
Wilsn12 <wtanjaya22@gmail.com>
2026-09-23 00:41:04 +0700

    Update contact section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,6 @@ function Navbar() {
         <a href="#home" onClick={() => setIsOpen(false)}>Home</a>
         <a href="#about" onClick={() => setIsOpen(false)}>About</a>
         <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
-        <a href="#experience" onClick={() => setIsOpen(false)}>Experience</a>
         <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
       </div>
 
